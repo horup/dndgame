c122cdfeaa66f175071237c9843da37f73cebba4
Horup <sah@sah.dk>
2020-04-15 21:30:47 +0200

    readded turn indication

--- client/handlers/index.ts
+++ client/handlers/index.ts
@@ -9,6 +9,7 @@ export const renderHandler:Handler<State, Command, Context> = (s, c, p, o, conte
     if (c.clientTick)
     {
         Object.entries(s.creatures).forEach(([id, creature])=>{
+            const hasTurn = s.turn != null && s.turn.creatureId == id;
             sprites.setSprites({
                 [id]:{
                     atlas:creature.class1,
@@ -16,7 +17,8 @@ export const renderHandler:Handler<State, Command, Context> = (s, c, p, o, conte
                     x:creature.x,
                     y:creature.y,
                     zIndex:0,
-                    anchor:{x:0.5, y:0.75}
+                    anchor:{x:0.5, y:0.75},
+                    tint:hasTurn ? 0xFFFFFF : 0xA0A0A0
                 }
             })
         });
